7af3d861bef930149b03d80480bb517fe9663ed2
Fleming <16278383@qq.com>
2018-06-22 16:03:03 +0800

    完成学生管理，课程管理，菜单测试成功

--- StudentGeneralMgr.cpp
+++ StudentGeneralMgr.cpp
@@ -28,10 +28,10 @@ class Course {
 		~Course(){}
 		
 		string toString(){
-			return 	"courseId："	+this->courseId		+","+
-					"courseCode："	+this->courseCode	+","+
-					"courseName："	+this->courseName	+","+
-					"courseT："		+this->courseT;
+			return 	"选课编号："+this->courseId		+","+
+					"课程代码："+this->courseCode	+","+
+					"课程名称："+this->courseName	+","+
+					"任课教师："+this->courseT;
 		}
 		
 		void setCourseId(string courseId){
@@ -83,9 +83,9 @@ class Score{
 		~Score(){}
 		
 		string toString(){
-			return 	"s_name："		+this->s_name		+","+
-					"courseName："	+this->courseName	+","+
-					"mark："		+this->mark;
+			return 	"学生姓名："+this->s_name		+","+
+					"课程："	+this->courseName	+","+
+					"成绩："	+this->mark;
 		}
 		
 		void setS_name(string s_name){
@@ -129,10 +129,10 @@ class Student{
 		~Student(){}
 		
 		string toString(){
-			return 	"id："		+this->s_id		+","+
-					"name："	+this->s_name	+","+
-					"sex："		+this->s_sex	+","+
-					"class："	+this->s_class; 
+			return 	"学号："+this->s_id		+","+
+					"姓名："+this->s_name	+","+
+					"性别："+this->s_sex	+","+
+					"班级："+this->s_class; 
 		}
 		
 		void setS_id(string s_id){
@@ -335,7 +335,736 @@ class StudentSvcImpl: public StudentSvc{
 	
 };
 
+//------------------------------------------------------------------------------
+
+void studentMgr();
+void courseMgr();
+void scoreMgr();
+void scoreAys(); 
+
+void studentAdd(); 
+void studentQuery();
+void studentQueryAll();
+void studentModify();
+void studentRemove();
+void studentFileo();
+void studentFilei();
+
+void courseAdd(); 
+void courseQuery();
+void courseQueryAll();
+void courseModify();
+void courseRemove();
+void courseFileo();
+void courseFilei();
+
+void zhuCaiDan(){
+	
+	system("cls"); 
+	cout<<"\n\n\n";
+	cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                     首页                        ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                  ◆1.学生管理                   ||"<<endl;  
+    cout<<"\t||                  ◆2.课程管理                   ||"<<endl;  
+    cout<<"\t||                  ◆3.成绩管理                   ||"<<endl;
+    cout<<"\t||                  ◆4.成绩分析                   ||"<<endl;  
+    cout<<"\t||                  ◆0.退出                       ||"<<endl;
+	cout<<"\t||                                                 ||"<<endl;
+	cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+	cout<<"\t-----------------------------------------------------"<<endl; 
+	
+	char x;      
+    bool flag = true;  
+    while(flag)  
+    {                      
+        x = getch();  
+        system("cls");  
+        fflush(stdin);  
+        switch(x)  
+        {  
+            case '1':  
+				studentMgr(); 
+                break;  
+            case '2':  
+           		courseMgr();
+                break;  
+            case '3':  
+            	//scoreMgr();
+                break;  
+            case '4':  
+              	//scoreAys();
+                break;  
+			case '0':
+				exit(0);
+            default: 
+				zhuCaiDan();  
+                break;  
+        }  
+	} 
+	
+};
+
+//------------------------------------------------------------------------------
+
+void studentMgr(){
+	
+	system("cls"); 
+	cout<<"\n\n\n";
+	cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                     学生管理                    ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                  ◆1.添加学生                   ||"<<endl;  
+    cout<<"\t||                  ◆2.查询学生                   ||"<<endl;  
+    cout<<"\t||                  ◆3.全体学生信息浏览           ||"<<endl;  
+    cout<<"\t||                  ◆4.修改学生                   ||"<<endl;  
+    cout<<"\t||                  ◆5.删除学生                   ||"<<endl;  
+    cout<<"\t||                  ◆6.保存（文本文件）           ||"<<endl; 
+	cout<<"\t||                  ◆7.读取数据（文本文件）       ||"<<endl;
+	cout<<"\t||                  ◆0.返回                       ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+	cout<<"\t-----------------------------------------------------"<<endl; 
+	
+	char x;      
+    bool flag = true;  
+    while(flag)  
+    {                      
+        x = getch();  
+        system("cls");  
+        fflush(stdin);  
+        switch(x)  
+        {  
+            case '1':  
+				studentAdd();
+                break;  
+            case '2':  
+           		studentQuery();
+                break;  
+            case '3':  
+            	studentQueryAll();
+                break;  
+            case '4':  
+              	studentModify();  
+                break;  
+            case '5':  
+                studentRemove();
+                break; 
+			case '6':
+				studentFileo(); 
+				break;
+			case '7':
+				studentFilei();
+				break;
+			case '0':
+				zhuCaiDan();
+            default: 
+				studentMgr(); 
+                break;  
+        }  
+	} 
+	
+}
+
+//------------------------------------------------------------------------------
+
+void studentAdd(){
+	Student student;
+	StudentSvcImpl studentSvc;
+	string s_id;
+	string s_name;
+	string s_sex;
+	string s_class;
+	
+	char flag='0';
+	while(flag == '0'){
+		cout<<"\n输入学生信息："<<endl;
+		
+		cout<<"输入学号：";
+		cin>>s_id;
+		
+		cout<<"输入姓名："; 
+		cin>>s_name; 
+		
+		cout<<"输入性别：";
+		cin>>s_sex;
+		
+		cout<<"输入班级：";
+		cin>>s_class;
+	
+		student.setS_id(s_id);
+		student.setS_name(s_name);
+		student.setS_sex(s_sex);
+		student.setS_class(s_class);
+		
+		studentSvc.addStudent(student);
+		
+		cout<<"输入结束,是否继续输入,按 0 键继续,否则退出。"<<endl;    
+        flag = getch();                                         
+        fflush(stdin);
+	}
+	
+	studentMgr();
+	
+}; 
+
+//------------------------------------------------------------------------------
+
+void studentQuery(){
+	
+	string s_id;
+	Student student;
+	StudentSvcImpl studentSvc;
+	
+	char flag='0';
+	while(flag == '0'){
+		
+		cout<<"\n\n输入要查询学生的学号：";
+		cin>>s_id;
+		student = studentSvc.queryStudent(s_id);
+		
+		if(student.getS_id() == "00000000"){
+			cout<<"\n查询的学生不存在,是否继续查询,按 0 键继续,否则退出。";
+			flag = getch();
+			fflush(stdin);
+		} else {
+			cout<<"\n"+student.toString()<<endl;
+			cout<<"\n查询结束,是否继续查询,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		}
+		
+	}
+	
+	studentMgr();
+	
+};
+
+//------------------------------------------------------------------------------
+
+void studentQueryAll(){
+		
+	char flag;
+	while(true){
+		
+		map<string, Student>::iterator iter;
+		for(iter = studentMap.begin(); iter != studentMap.end(); iter++) {
+			cout<<"\n"+iter->second.toString()<<endl;   		
+		}
+		
+		cout<<"\n查询结束,输入任意键退出。"<<endl;    
+        flag = getch();                                         
+        break; 
+	} 
+	
+	studentMgr(); 
+	
+};
+
+//------------------------------------------------------------------------------
+
+void studentModify(){
+	
+	string s_id;
+	string s_name;
+	string s_sex;
+	string s_class;
+	
+	Student student;
+	StudentSvcImpl studentSvc;
+	
+	char flag='0';
+	while(flag == '0'){
+		
+		cout<<"\n输入要修改学生的学号：";
+		cin>>s_id;
+		
+		student = studentSvc.queryStudent(s_id);
+		
+		if(student.getS_id() == "00000000"){
+			cout<<"\n想要修改的学生不存在,是否继续修改,按 0 键继续,否则退出。";
+			flag = getch();
+			fflush(stdin);
+		} else {
+			
+			cout<<"\n\n输入要修改的信息\n"<<endl;
+			
+			cout<<"输入修改后的姓名："; 
+			cin>>s_name; 
+		
+			cout<<"输入修改后的性别：";
+			cin>>s_sex; 
+		
+			cout<<"输入修改后的班级：";
+			cin>>s_class;
+			
+			student.setS_id(s_id);
+			student.setS_name(s_name);
+			student.setS_sex(s_sex);
+			student.setS_class(s_class);
+			
+			studentSvc.modifyStudent(student); 
+			
+			cout<<"\n修改结束,是否继续修改,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		}
+		
+	} 
+	
+	studentMgr();
+	
+};
+
+//------------------------------------------------------------------------------ 
+
+void studentRemove(){
+	
+	string s_id;	
+	StudentSvcImpl studentSvc;
+	
+	char flag='0';
+	while(flag == '0'){
+		
+		cout<<"\n\n输入要删除学生的id：";
+		cin>>s_id;
+		
+		int n = studentSvc.removeStudent(s_id);
+		
+		if(n == 1){
+			cout<<"\n删除成功,是否继续删除,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		} else {
+			cout<<"\n删除失败,是否继续删除,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		}
+			 
+	}
+
+	studentMgr();
+	
+};
+
+//------------------------------------------------------------------------------
+
+void studentFilei(){			//读取数据 
+	
+	ifstream infile; 
+	infile.open("data\\学生信息.txt",ios::in); 
+	string s;
+	
+	string s_id;
+	string s_name;
+	string s_sex;
+	string s_class;
+	
+	Student student;
+	StudentSvcImpl studentSvc;
+	
+	char flag='0';
+	while(true){
+		
+		while(getline(infile,s)){
+			
+			istringstream ss(s);
+			
+			ss>>s_id;
+			ss>>s_name;
+			ss>>s_sex;
+			ss>>s_class;
+			
+			student.setS_id(s_id);
+			student.setS_name(s_name);
+			student.setS_sex(s_sex);
+			student.setS_class(s_class);
+			
+			studentSvc.addStudent(student);
+		}
+		
+		infile.close();
+		cout<<"读取成功，按任意键退出"<<endl;    
+        flag = getch();
+        break; 
+	}
+	
+	studentMgr();
+	
+};
+
+//------------------------------------------------------------------------------
+
+void studentFileo(){			//写入数据(写入之前清空文件)
+	
+	ofstream ofile;
+	ofile.open("data\\学生信息.txt", ios::out | ios::trunc );
+	
+	char flag='0';
+	while(true){
+	
+		map<string, Student>::iterator iter;
+		for(iter = studentMap.begin(); iter != studentMap.end(); iter++) {
+			
+			ofile<<	iter->second.getS_id() 		+ "\t"<<
+					iter->second.getS_name() 	+ "\t"<<
+					iter->second.getS_sex() 	+ "\t"<<
+					iter->second.getS_class() 	+ "\t"<<endl;   		
+		
+		}
+	
+		ofile.close();
+		cout<<"写入成功，按任意键退出"<<endl;    
+        flag = getch();
+       	break; 
+	}
+	
+	studentMgr(); 
+	
+};
+
+//------------------------------------------------------------------------------
+
+void courseMgr(){
+	
+	system("cls"); 
+	cout<<"\n\n\n";
+	cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                     课程管理                    ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                  ◆1.添加课程                   ||"<<endl;  
+    cout<<"\t||                  ◆2.查询课程                   ||"<<endl;  
+    cout<<"\t||                  ◆3.所有课程信息浏览           ||"<<endl;  
+    cout<<"\t||                  ◆4.修改课程                   ||"<<endl;  
+    cout<<"\t||                  ◆5.删除课程                   ||"<<endl;  
+    cout<<"\t||                  ◆6.保存（文本文件）           ||"<<endl; 
+	cout<<"\t||                  ◆7.读取数据（文本文件）       ||"<<endl;
+	cout<<"\t||                  ◆0.返回                       ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t||                                                 ||"<<endl;  
+    cout<<"\t-----------------------------------------------------"<<endl;  
+	cout<<"\t-----------------------------------------------------"<<endl; 
+	
+	char x;      
+    bool flag = true;  
+    while(flag)  
+    {                      
+        x = getch();  
+        system("cls");  
+        fflush(stdin);  
+        switch(x)  
+        {  
+            case '1':  
+				courseAdd();
+                break;  
+            case '2':  
+           		courseQuery();
+                break;  
+            case '3':  
+            	courseQueryAll();
+                break;  
+            case '4':  
+              	courseModify();  
+                break;  
+            case '5':  
+                courseRemove();
+                break; 
+			case '6':
+				courseFileo(); 
+				break;
+			case '7':
+				courseFilei();
+				break;
+			case '0':
+				zhuCaiDan();
+            default: 
+				courseMgr(); 
+                break;  
+        }  
+	} 
+	
+}
+
+//------------------------------------------------------------------------------
+
+void courseAdd(){
+	CourseSvcImpl courseSvc;
+	Course course;
+	string courseId;
+	string courseCode;
+	string courseName;
+	string courseT;
+	
+	char flag='0';
+	while(flag == '0'){
+		cout<<"\n输入课程信息："<<endl;
+		
+		cout<<"输入选课编号：";
+		cin>>courseId;
+		
+		cout<<"输入课程代码："; 
+		cin>>courseCode; 
+		
+		cout<<"输入课程名称：";
+		cin>>courseName;
+		
+		cout<<"输入任课老师：";
+		cin>>courseT;
+	
+		course.setCourseId(courseId);
+		course.setCourseCode(courseCode);
+		course.setCourseName(courseName);
+		course.setCourseT(courseT); 
+		
+		courseSvc.addCourse(course);
+		
+		cout<<"输入结束,是否继续输入,按 0 键继续,否则退出。"<<endl;    
+        flag = getch();                                         
+        fflush(stdin);
+	}
+	
+	courseMgr();
+	
+}; 
+
+//------------------------------------------------------------------------------
+
+void courseQuery(){
+	
+	string courseId;
+	Course course;
+	CourseSvcImpl courseSvc;
+	
+	char flag='0';
+	while(flag == '0'){
+		
+		cout<<"\n\n输入要查询课程的选课编号：";
+		cin>>courseId;
+		course = courseSvc.queryCourse(courseId);
+		
+		if(course.getCourseId() == "00000000"){
+			cout<<"\n查询的课程不存在,是否继续查询,按 0 键继续,否则退出。";
+			flag = getch();
+			fflush(stdin);
+		} else {
+			cout<<"\n"+course.toString()<<endl;
+			cout<<"\n查询结束,是否继续查询,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		}
+		
+	}
+	
+	courseMgr();
+	
+};
+
+//------------------------------------------------------------------------------
+
+void courseQueryAll(){
+		
+	char flag;
+	while(true){
+		
+		map<string, Course>::iterator iter;
+		for(iter = courseMap.begin(); iter != courseMap.end(); iter++) {
+			cout<<"\n"+iter->second.toString()<<endl;   		
+		}
+		
+		cout<<"\n查询结束,输入任意键退出。"<<endl;    
+        flag = getch();                                         
+        break; 
+	} 
+	
+	courseMgr();
+	
+};
+
+//------------------------------------------------------------------------------
+
+void courseModify(){
+	
+	string courseId;
+	string courseCode;
+	string courseName;
+	string courseT;
+	
+	Course course; 
+	CourseSvcImpl courseSvc;
+	
+	char flag='0';
+	while(flag == '0'){
+		
+		cout<<"\n输入要修改课程的选课编号：";
+		cin>>courseId;
+		
+		course = courseSvc.queryCourse(courseId);
+		
+		if(course.getCourseId() == "00000000"){
+			cout<<"\n想要修改的课程不存在,是否继续修改,按 0 键继续,否则退出。";
+			flag = getch();
+			fflush(stdin);
+		} else {
+			
+			cout<<"\n\n输入要修改的信息\n"<<endl;
+			
+			cout<<"输入修改后的课程代码："; 
+			cin>>courseCode; 
+		
+			cout<<"输入修改后的课程名称：";
+			cin>>courseName;
+		
+			cout<<"输入修改后的任课老师：";
+			cin>>courseT;
+			
+			course.setCourseId(courseId);
+			course.setCourseCode(courseCode);
+			course.setCourseName(courseName);
+			course.setCourseT(courseT);
+			
+			courseSvc.modifyCourse(course);
+			
+			cout<<"\n修改结束,是否继续修改,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		}
+		
+	} 
+	
+	courseMgr();
+	
+};
+
+//------------------------------------------------------------------------------
+
+void courseRemove(){
+	
+	string courseId;	
+	CourseSvcImpl courseSvc;
+	
+	char flag='0';
+	while(flag == '0'){
+		
+		cout<<"\n\n输入要删除课程的选课编号：";
+		cin>>courseId;
+		
+		int n = courseSvc.removeCourse(courseId);
+		
+		if(n == 1){
+			cout<<"\n删除成功,是否继续删除,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		} else {
+			cout<<"\n删除失败,是否继续删除,按 0 键继续,否则退出。"<<endl;    
+        	flag = getch();                                         
+        	fflush(stdin);
+		}
+			 
+	}
+
+	courseMgr();
+	
+};
+
+//------------------------------------------------------------------------------ 
+
+void courseFilei(){			//读取数据 
+	
+	ifstream infile; 
+	infile.open("data\\课程信息.txt",ios::in); 
+	string s;
+	
+	string courseId;
+	string courseCode;
+	string courseName;
+	string courseT;
+	
+	Course course; 
+	CourseSvcImpl courseSvc;
+	
+	char flag='0';
+	while(true){
+		
+		while(getline(infile,s)){
+			
+			istringstream ss(s);
+			
+			ss>>courseId;
+			ss>>courseCode;
+			ss>>courseName;
+			ss>>courseT;
+			
+			course.setCourseId(courseId);
+			course.setCourseCode(courseCode);
+			course.setCourseName(courseName);
+			course.setCourseT(courseT);
+			
+			courseSvc.addCourse(course);
+		}
+		
+		infile.close();
+		cout<<"读取成功，按任意键退出"<<endl;    
+        flag = getch();
+        break; 
+	}
+	
+	courseMgr();
+	
+};
+
+//------------------------------------------------------------------------------
+
+void courseFileo(){			//写入数据(写入之前清空文件)
+	
+	ofstream ofile;
+	ofile.open("data\\课程信息.txt", ios::out | ios::trunc );
+	
+	char flag='0';
+	while(true){
+	
+		map<string, Course>::iterator iter;
+		for(iter = courseMap.begin(); iter != courseMap.end(); iter++) {
+			
+			ofile<<	iter->second.getCourseId() 		+ "\t"<<
+					iter->second.getCourseCode()	+ "\t"<<
+					iter->second.getCourseName() 	+ "\t"<<
+					iter->second.getCourseT() 		+ "\t"<<endl;   		
+		
+		}
+	
+		ofile.close();
+		cout<<"写入成功，按任意键退出"<<endl;    
+        flag = getch();
+       	break; 
+	}
+	
+	courseMgr();
+	
+}; 
+
 int main(){
+	
+	zhuCaiDan();
 	
 	return 0;
 }
